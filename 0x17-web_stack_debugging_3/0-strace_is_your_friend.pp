0f2637b9bc3a8eee080da567289189cb63b88ce7
penality <trigger_austin00@proton.me>
2024-08-15 00:00:57 +0300

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # automated fix for apache fail
 
 exec {'fix wordpress site':
-	command => 'sudo sed -i "s/phpp/php/g" /var/www/html/wq-settings.php',
+	command => 'sudo sed -i s/phpp/php/g /var/www/html/wq-settings.php',
 	path => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }

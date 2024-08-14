6b3aea478eec62999f8286027f1237049378de2e
penality <trigger_austin00@proton.me>
2024-08-15 00:28:21 +0300

    fixed err in sandbox

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,24 @@
 # automated fix for apache fail
 
-exec {'fix wordpress site':
-    command => 'sudo sed -i s/phpp/php/g /var/www/html/wq-settings.php',
-    path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+file { '/var/www/html/wp-settings.php':
+  ensure  => file,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  source  => 'puppet:///modules/mymodule/wp-settings.php', # Ensure this file is managed by Puppet
+  notify  => Service['apache2'],
 }
+
+exec { 'fix_wordpress_inclusion':
+  command => 'sed -i "s/class-wp-locale.phpp/class-wp-locale.php/" /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin'],
+  unless  => 'grep -q "class-wp-locale.php" /var/www/html/wp-settings.php',
+  notify  => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure    => running,
+  enable    => true,
+  subscribe => Exec['fix_wordpress_inclusion'], # Restart Apache if the exec changes the file
+}
+
